29b36eff181cdef1bc38f8ce9d953388aa510ce7
NesmashIgor <116344696+NesmashIgor@users.noreply.github.com>
2023-04-12 22:35:47 +0300

    ДЗ 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const ulGallery = document.querySelector('.gallery')
+
+function renderItem(item) {
+    ulGallery.insertAdjacentHTML('afterbegin', `<li class="gallery__item">
+    <a class="gallery__link" href= ${item.original}>
+        <img
+            class="gallery__image"
+            src= ${item.preview}
+            data-source=${item.original}
+            alt=${item.description} 
+        />
+    </a>
+</li>`)
+}
+
+galleryItems.forEach((item) => {
+    renderItem(item)
+})
+
+
+
+ulGallery.addEventListener('click', (event) => {
+    event.preventDefault()
+    let target = event.target
+
+    if (target.className === "gallery__image") {
+
+        const instance = basicLightbox.create(`
+        <img      
+        class="gallery__image"
+        src= ${target.dataset.source}
+         
+         />
+         
+      `)
+        instance.show()
+    if(instance.show() === true) {
+        ulGallery.addEventListener( 'keydown', (event)=> {
+            if(event.code === 'Escape') {
+                instance.close()
+            }
+        })
+    }
+    }
+
+})
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+const ulGallery = document.querySelector(".gallery");
+
+function renderItem(item) {
+  ulGallery.insertAdjacentHTML(
+    "afterbegin",
+    `<li class="gallery__item">
+<a class="gallery__link" href= "${item.original}">
+    <img
+        title = "${item.description}"
+        class="gallery__image"
+        src= "${item.preview}"
+        alt="${item.description}"
+    />
+</a>
+</li>`
+  );
+}
+
+galleryItems.forEach((item) => {
+  renderItem(item);
+});
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250
+});
+gallery.on("show.simplelightbox", function () {});
+
+console.log(gallery);
